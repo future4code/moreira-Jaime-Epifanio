641cdc6ef15c8f54f90827f6e752b666bf7c1f3d
jaime-github <jaime-epifanio@hotmail.com>
2021-12-02 18:27:21 -0300

    Exercício_Funções

---
+++ modulo-1/funcoes/Exercício_3/index.js
@@ -0,0 +1,39 @@
+
+
+
+const numerosUsuario1 = Number(prompt("Por Favor, digite um número"))
+const numerosUsuario2 = Number(prompt("Por Favor, digite outro número"))
+
+console.log(`Números inseridos: ${numerosUsuario1}, ${numerosUsuario2}`)
+
+const somaDoisNumeros = (numero1, numero2) => {
+return numero1 + numero2
+
+ }
+ console.log(`Soma: ${somaDoisNumeros(numerosUsuario1, numerosUsuario2)}`)  
+
+const subtraiDoisNumeros = (numero1, numero2) => {
+return numero1 - numero2
+    
+
+ }
+ console.log(`Subtração: ${subtraiDoisNumeros(numerosUsuario1, numerosUsuario2)}`)
+
+const multiplicaoDoisNumeros = (numero1, numero2) => {
+return numero1 * numero2
+    
+
+ }
+ console.log(`Multiplicação: ${multiplicaoDoisNumeros(numerosUsuario1,numerosUsuario2)}`)
+
+const divisaoDoisNumeros = (numero1, numero2) => {
+return numero1 / numero2
+
+
+}
+console.log(`Divisão: ${divisaoDoisNumeros(numerosUsuario1,numerosUsuario2)}`)
+
+
+
+
+ 
