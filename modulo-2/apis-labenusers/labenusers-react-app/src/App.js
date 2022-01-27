4e7eab1ed410ccce034973e7722e60cc2f4d2736
jaime-github <jaime-epifanio@hotmail.com>
2022-01-27 00:06:22 -0300

    Labenusers-Versão-Final

--- modulo-2/apis-labenusers/labenusers-react-app/src/App.js
+++ modulo-2/apis-labenusers/labenusers-react-app/src/App.js
@@ -1,90 +1,40 @@
-import axios from "axios";
 import React from "react";
-
-const urlCadastroUsers =
-  "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
-
-const headers = {
-  headers: {
-    Authorization: "jaime-epifanio-moreira"
-  }
-};
+import TelaCadastro from "./components/TelaCadastro";
+import TelaListaUsuarios from "./components/TelaListaUsuarios";
 
 export default class App extends React.Component {
   state = {
-    usuarios: [],
-    nomeUserInput: "",
-    emailUserInput: ""
+    telaAtual: "lista",
   };
 
-  componentDidMount() {
-    this.getAllUsers();
+  escolheTela = () => {
+    switch (this.state.telaAtual) {
+      case "cadastro":
+        return <TelaCadastro irParaLista = {this.irParaLista} />
+      case "lista":
+        return <TelaListaUsuarios irParaCadastro = {this.irParaCadastro} />
+        default:
+          return <div>Erro! Página não Encontrada!</div>
+    }
+  }    
+
+  irParaCadastro = () => {
+    this.setState({ telaAtual: "cadastro" })
   }
 
-  getAllUsers = () => {
-    axios
-      .get(urlCadastroUsers, headers)
-      .then((res) => {
-        this.setState({ usuarios: res.data });
-      })
-      .catch((err) => {
-        alert("Ops, algo de errado ocorreu, tente novamente!");
-      });
-  };
-
-  createUsers = () => {
-    const body = {
-      name: this.state.nomeUserInput,
-      email: this.state.emailUserInput,
-    };
-
-    axios
-      .post(urlCadastroUsers, body, headers)
-      .then((res) => {
-        alert(`Usuário ${this.state.nomeUserInput} foi criado com sucesso!`);
-        this.setState({ nomeUserInput: "" });
-        this.setState({ emailUserInput: "" });
-        this.getAllUsers();
-      })
-      .catch((err) => {
-        alert(err.response.data.message);
-        this.setState({ nomeUserInput: "" });
-      });
-  };
-
-  onTypeNameChange = (event) => {
-    this.setState({ nomeUserInput: event.target.value });
-  };
-
-  onTypeNameEmail = (event) => {
-    this.setState({ emailUserInput: event.target.value });
-  };
+  irParaLista = () => {
+    this.setState({ telaAtual: "lista" });
+  }
 
   render() {
-    const usersComponents = this.state.usuarios.map((cad) => {
-      return <li key={cad.id}>{cad.name}</li>;
-    });
-
     return (
-      <div>
-        <h1>Labenusers</h1>
-        {usersComponents}
-        
-        <input
-          value={this.state.nomeUserInput}
-          onChange={this.onTypeNameChange}
-          placeholder="Seu Nome"
-        />
-        <input
-          value={this.state.emailUserInput}
-          onChange={this.onTypeNameEmail}
-          placeholder="Seu e-mail"
-        />
-        <button onClick={this.createUsers}>Adicionar Usuários</button>
-        <div>
-        <button>Tela Adicional</button>
-        </div>
+      <div> 
+          {this.escolheTela()}
       </div>
-    );
+      )
+    }
   }
-}
+
+
+
+
