cdeb61089cd80560064a663ec52a7ad1dae5da90
jaime-github <jaime-epifanio@hotmail.com>
2022-02-25 22:42:49 -0300

    Projeto-Labeddit-Versão2

---
+++ modulo-3/projeto-labeddit/labeddit/src/Pages/Feeds/index.jsx
@@ -0,0 +1,42 @@
+// import React from "react";
+// import { HeaderSite, Logo } from "../../Components/HeaderAll/styled";
+// import { QuadranteFeed } from "../Feeds/styled"
+// import { Input, Space } from "antd";
+
+
+// const onSearch = (value) => console.log(value);
+
+// export default function Feeds() {
+//   const { Search } = Input;
+
+//   const suffix = (
+//     <AudioOutlined
+//       style={{
+//         fontSize: 16,
+//         color: "#1890ff",
+        
+//       }}
+//     />
+//   );
+//   return (
+//     <HeaderSite>
+//       <Logo src="https://i.postimg.cc/3wWpMH8Q/Labeddit-1.png" />
+//       <Space direction="vertical">
+//         <Search
+//           placeholder="input search text"
+//           onSearch={onSearch}
+//           enterButton
+//           style={{
+//             width: 900,
+//             paddingLeft: 50,
+            
+//           }}
+//         />
+//         <QuadranteFeed/>
+//       </Space>
+//       ,
+//     </HeaderSite>
+//   );
+// }
+
+
---
+++ modulo-3/projeto-labeddit/labeddit/src/Pages/Login/styled.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+
+export const Papai = styled.div`
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top:30px;
+    width:600px;
+    height:550px;
+    /* background-color:#67A19D;  */
+    color:black;
+    border-radius: 10px;
+`
+
+export const FormGeral = styled.div`
+    flex-wrap: nowrap;
+    flex-direction: column;
+    align-items: center;
+    width: 400px;
+        
+`
+
+export const LogoTipo = styled.img`
+    position: relative;
+    width:300px;    
+    padding-top: 30px;
+    padding-bottom: 30px;
+`
+
+export const BotaoEntrar = styled.button`
+    flex-wrap: nowrap;
+    flex-direction: column;
+    align-items: center;
+    width: 160px;
+    height: 40px;
+    background-color: #fe3e17;
+    color: black;
+    border:none;
+    cursor: pointer;
+    font-size: 20px;
+`
+
+export const P = styled.p`
+    text-align: center;
+    width: 100%;
+    padding-top: 10px;
+    padding-right: 100px;
+`
+
+
+
+
