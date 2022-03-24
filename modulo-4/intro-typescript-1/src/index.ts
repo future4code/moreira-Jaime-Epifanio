2e58d97cc6d858def5c38a814bfcb90b0ba72814
jaime-github <jaime-epifanio@hotmail.com>
2022-03-23 21:36:23 -0300

    Exercício-intro-typescript-1

---
+++ modulo-4/intro-typescript-1/src/index.ts
@@ -0,0 +1,10 @@
+export function checaTriangulo(a: number, b: number, c: number): string {
+    if (a !== b && b !== c) {
+      return "Escaleno";
+    } else if (a === b && b === c) {
+      return "Equilátero";
+    } else {
+      return "Isósceles";
+    }
+}
+console.log(checaTriangulo(4,5,7))
